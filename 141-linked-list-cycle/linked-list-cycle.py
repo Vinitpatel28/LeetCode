8a14b3c4646e64300acc3ab084ab031c2c3e8af7
Vinitpatel28 <vinitpatelrajeshbhai@gmail.com>
2025-06-07 11:09:44 +0530

    Time: 49 ms (51.31%) | Memory: 20 MB (16.45%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,35 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+    
+        if not head or not head.next:
+            return False
+        
+        slow = head
+        fast = head.next
+        
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
+
+if __name__ == "__main__":
+    node1 = ListNode(3)
+    node2 = ListNode(2)
+    node3 = ListNode(0)
+    node4 = ListNode(-4)
+    
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2  
+    
+    solution = Solution()
+    print(solution.hasCycle(node1))  
